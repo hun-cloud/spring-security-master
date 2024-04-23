fb0c7bc57f41f30985a6027d2442c6b1b15ba16a
leejaehun <ljh0901@mlsoft.com>
2024-04-23 12:23:43 +0900

    시큐리티 필터체인 빈 생성

--- src/main/java/io/security/springsecuritymaster/security/configs/SecurityConfig.java
+++ src/main/java/io/security/springsecuritymaster/security/configs/SecurityConfig.java
@@ -5,30 +5,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeHttpRequests(auth -> auth
-//                        .requestMatchers("/").permitAll()
-//                        .anyRequest().authenticated())
-//                .formLogin(Customizer.withDefaults())
-//        ;
-//        return http.build();
-//    }
-//
-//    @Bean
-//    public UserDetailsService userDetailsService(){
-//        UserDetails user = User.withUsername("user").password("{noop}1111").roles("USER").build();
-//        return  new InMemoryUserDetailsManager(user);
-//    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults());
+        return http.build(); // 시큐리티 필터체인 빈 생성
+    }
 }
