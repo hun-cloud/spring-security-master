6b7c1175077fb2153d1b85ce499896d3b4ec86f6
leejaehun <ljh0901@mlsoft.com>
2025-02-10 10:05:28 +0900

    rememberme

--- src/main/java/io/security/springsecuritymaster/security/config/SecurityConfig.java
+++ src/main/java/io/security/springsecuritymaster/security/config/SecurityConfig.java
@@ -20,14 +20,15 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-                .httpBasic(basic -> basic.authenticationEntryPoint(new CustomAuthenticationEntryPoint()));
-
-//                        basic -> basic.authenticationEntryPoint( <- 커스텀 가능함
-//                        (request, response, authException) -> {
-//                            response.setHeader("WWW-Authenticate", "Basic realm=security");
-//                            response.sendError(HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED.getReasonPhrase());
-//                        }
-//                ));
+                .formLogin(Customizer.withDefaults())
+                .rememberMe(rememberMe -> rememberMe
+                        .alwaysRemember(true) // 기억하기(remember-me) 매개변수가 설정되지 않았을 때에도 쿠키가 항상 생성되어야 하는지에 대한 여부를 나타낸다.
+                        .tokenValiditySeconds(3600) // 토큰이 유요한 시간(초 단위)를 저장할 수 있다)
+                        .userDetailsService(userDetailsService()) // UserDetails를 조회하기 위해 사용되는 UserDetailsService를 지정한다.
+                        .rememberMeParameter("remember") // 로그인 시 사용자를 기억하기 위해 사용되는 HTTP 매개변수이며 기본적으로 remember-me이다.
+                        .rememberMeCookieName("remember") // 기억하기(remember-me) 인증을 위한 토큰을 저장하는 쿠키 이름이며 기본 값은 remember-me 이다.
+                        .key("security") // 기억하기(remember-me) 인증을 위해 생성된 토큰을 식별하는 키를 설정한다.
+                );
 
         return http.build();
     }
